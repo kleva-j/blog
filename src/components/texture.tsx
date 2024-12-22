b6468baea1c0b7be9d858970c32e4c33c0e1c6e0
kleva-j <kasmickleva@gmail.com>
2024-12-22 01:32:29 +0100

    feat: render svg page texture and mode-toggle components

---
+++ src/components/texture.tsx
@@ -0,0 +1,14 @@
+export const SvgTexture = ({ className }: { className?: string }) => (
+  <svg id="texture" className={className}>
+    <filter id="noise">
+      <feTurbulence
+        type="fractalNoise"
+        baseFrequency=".8"
+        numOctaves="4"
+        stitchTiles="stitch"
+      ></feTurbulence>
+      <feColorMatrix type="saturate" values="0"></feColorMatrix>
+    </filter>
+    <rect width="100%" height="100%" filter="url(#noise)"></rect>
+  </svg>
+);
