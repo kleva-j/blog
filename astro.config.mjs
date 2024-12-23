2d7978718646828fc6f5892fa5e4671d5d2723af
kleva-j <kasmickleva@gmail.com>
2024-12-23 09:38:39 +0100

    feat: enable page-insight analysis

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import pageInsight from "astro-page-insight";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  integrations: [tailwind({ applyBaseStyles: false }), react(), pageInsight()],
 });
