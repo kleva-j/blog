034fb7a1e299688ce90848cf9bf45ca6865c2cb3
kleva-j <kasmickleva@gmail.com>
2025-01-28 11:45:09 +0100

    refactor: icon toggle component; use template string for dynamic string values

--- src/components/icon-toggle.tsx
+++ src/components/icon-toggle.tsx
@@ -1,13 +1,8 @@
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
-import { cn } from "@/lib/utils";
 
-interface IconToggleProps {
-  onToggle?: (toggled: boolean) => void;
-}
-
-export const IconToggle = (props: IconToggleProps) => {
+export const IconToggle = () => {
   const [theme, setThemeState] = useState<"theme-light" | "dark" | "system">(
     "theme-light",
   );
@@ -36,12 +31,7 @@ export const IconToggle = (props: IconToggleProps) => {
     <MotionConfig transition={{ duration: 0.2, type: "tween", ease: "easeIn" }}>
       <AnimatePresence initial={false}>
         <motion.button
-          className={cn(
-            "relative isolate flex items-center h-7 w-11 cursor-pointer border rounded-full duration-200",
-            !isLightMode
-              ? "bg-neutral-950/40 border-neutral-500/20"
-              : "bg-purple-950/20 border-transparent",
-          )}
+          className={`relative isolate flex items-center h-7 w-11 cursor-pointer border rounded-full duration-200 ${!isLightMode ? "bg-neutral-950/40 border-neutral-500/20" : "bg-purple-950/20 border-transparent"}`}
           onClick={handleClick}
           type="button"
         >
@@ -50,26 +40,18 @@ export const IconToggle = (props: IconToggleProps) => {
               x: isLightMode ? "7%" : "100%",
               scale: isLightMode ? 1 : 0.99,
             }}
-            className={cn(
-              "flex items-center justify-center rounded-full bg-white dark:bg-neutral-950/30 shadow-lg transition-all duration-200 size-5",
-            )}
+            className="flex items-center justify-center rounded-full bg-white dark:bg-neutral-950/30 shadow-lg transition-all duration-200 size-5"
           >
             <span className="relative grid place-items-center">
               <Sun
                 size={15}
                 strokeWidth={3}
-                className={cn(
-                  "h-full w-full p-1 text-gray-500 transition-opacity duration-200",
-                  isLightMode ? "opacity-100" : "opacity-0",
-                )}
+                className={`h-full w-full p-1 text-gray-500 transition-opacity duration-200 ${isLightMode ? "opacity-100" : "opacity-0"}`}
               />
               <Moon
                 size={13}
                 strokeWidth={2}
-                className={cn(
-                  "absolute left-0 top-0 h-full w-full p-[1px] text-gray-500 transition-opacity duration-200",
-                  isLightMode ? "opacity-0" : "opacity-100",
-                )}
+                className={`absolute left-0 top-0 h-full w-full p-[1px] text-gray-500 transition-opacity duration-200 ${isLightMode ? "opacity-0" : "opacity-100"}`}
               />
             </span>
           </motion.div>
